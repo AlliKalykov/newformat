1ee36697468cc3730006b8656cad7f1d569d12d0
AlliKalykov <alli.kalykov@gmail.com>
2019-06-27 17:09:08 +0600

    Corrected home page

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,14 @@ router.get('/', function (req, res, next) {
     image: ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
   }
   let restObj1 = {
-    restTitle: ["Заведение 1", "Заведение 2", "Заведение 3", "Заведение 4", "Заведение 5", "Заведение 6"],
-    facebook: ["https://facebook.com", "https://facebook.com", "https://facebook.com", "https://facebook.com", "https://facebook.com", "https://facebook.com"],
-    instagram: ["https://instagram.com", "https://instagram.com", "https://instagram.com", "https://instagram.com", "https://instagram.com", "https://instagram.com"],
-    restHref: ["https://instagram.com", "https://instagram.com", "https://instagram.com", "https://instagram.com", "https://instagram.com", "https://instagram.com"],
-    image: ["/images/t1.jpg", "/images/t2.jpg", "/images/t3.jpg", "/images/t4.jpg", "/images/t5.jpg", "/images/t6.jpg"]
+    restTitle: ["Заведение 1", "Заведение 2", "Заведение 3"],
+    facebook: ["https://facebook.com", "https://facebook.com", "https://facebook.com"],
+    instagram: ["https://instagram.com", "https://instagram.com", "https://instagram.com"],
+    restHref: ["https://instagram.com", "https://instagram.com", "https://instagram.com"],
+    image: ["/images/t1.jpg", "/images/t2.jpg", "/images/t3.jpg"]
   }
   
-  let catObj2 = [
+  let greatObj2 = [
     {
       href: "https://bootstrap-4.ru/",
       rekTitle: "bootstrap",
@@ -40,8 +40,28 @@ router.get('/', function (req, res, next) {
     }
   ]
 
+  let catObj1 = [
+    {
+      href: "https://bootstrap-4.ru/",
+      rekTitle: "Свадьбы",
+      rekText: "Свадьба, самое прекрасное в жизни человека",
+      image: "images/svadba.jpg"
+    },
+    {
+      href: "https://translate.google.ru/",
+      rekTitle: "Кыз узатуу",
+      rekText: "Самое трепетное событие для родителей девочек",
+      image: "images/kyz.jpg"
+    },
+    {
+      href: "https://support.mozilla.org/en-US/",
+      rekTitle: "Бешик той",
+      rekText: "Прекрасное событие для родителей",
+      image: "images/beshik.jpg"
+    }
+  ]
   let reklamaArr1 = ["images/blog2.jpg", "images/blog3.jpg"]
-  res.render('index', { title: 'Новый формат', sliderObj: obj1, restObj: restObj1, reklamaArr: reklamaArr1, greatObj: catObj2 });
+  res.render('index', { title: 'Новый формат', sliderObj: obj1, restObj: restObj1, reklamaArr: reklamaArr1, greatObj: greatObj2, catObj: catObj1 });
   
 });
 
@@ -52,4 +72,15 @@ router.post('/', (req, res, next) => {
   res.render("great", { greatObj: catObj2 })
 });
 
+router.get('/mobile', function (req, res) {
+  let obj1 = {
+    zag: ["Новый формат в предоставлении услуг фотографа!",
+    "Создание качественных фотографий с профессиональным прикосновением!",
+    "Заказывайте фотоотчеты ващего мероприятия!",
+    "Обращайтесь по следующим контактам!"
+    ], 
+    image: ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
+  }
+    res.render('mobile', { title: 'Новый формат', sliderObj: obj1});
+  });
 module.exports = router;
